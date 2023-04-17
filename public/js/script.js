c22ec7bf5f83770da682d82b091c0b525e10c4c7
Daniele Manca <danimanca2010@gmail.com>
2023-04-17 17:01:18 +0100

    Commit script changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,23 @@
 window.addEventListener("load", function() {
-	var basketDiv = document.getElementById("basket");
-	basketDiv.id = "basket";
-	basketDiv.innerHTML = `
-	  <select id="item-select">
-		<option value="item1">Item 1</option>
-		<option value="item2">Item 2</option>
-		<option value="item3">Item 3</option>
-	  </select>
-	  <button id="add-to-basket">Add to basket</button>
-	`;
-	document.body.appendChild(basketDiv);
-  
-	window.addEventListener("scroll", function() {
-	  var basket = document.getElementById("basket");
-	  if (window.pageYOffset > 0) {
-		basket.style.opacity = "1";
-	  } else {
-		basket.style.opacity = "0";
-	  }
-	});
+  var basketDiv = document.createElement("div");
+  basketDiv.id = "basket";
+  basketDiv.style.display = "none";
+  basketDiv.innerHTML = `
+    <select id="item-select">
+      <option value="item1">Item 1</option>
+      <option value="item2">Item 2</option>
+      <option value="item3">Item 3</option>
+    </select>
+    <button id="add-to-basket">Add to basket</button>
+  `;
+  document.body.appendChild(basketDiv);
+
+  window.addEventListener("scroll", function() {
+    var basket = document.getElementById("basket");
+    if (window.pageYOffset > 0) {
+      basket.style.opacity = "1";
+    } else {
+      basket.style.opacity = "0";
+    }
+  });
 });
-  
